488ad87ff8e039c02650a85a1fe8abe42f70d67d
data-niklas <niklas@4loeser.net>
2023-06-24 21:52:53 +0200

    add qol features

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,5 @@
+let split = import('./split-grid.js')
+
 function debounce(fn, timeout){
   let pending = null
   return function(){
@@ -6,7 +8,40 @@ function debounce(fn, timeout){
   }
 }
 
+function enable_tab(){
+  let textarea = document.getElementById('code')
+  textarea.addEventListener('keydown', e=>{
+  if (e.key == 'Tab') {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    textarea.value = textarea.value.substring(0, start) +
+      "\t" + textarea.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;
+  }
+  })
+}
+
+function enable_split(){
+  Split({
+    minSize: 300,
+      columnGutters: [{
+          track: 1,
+          element: document.querySelector('.gutter-col-1'),
+      }],
+  })
+}
+
+const TIMEOUT = 500
+
 document.addEventListener('wasmload', async function() {
+    enable_tab()
+  enable_split()
     let rust = await import(window.bindingsfile)
     let typst = new rust.SystemWorld();
     document.getElementById("code").addEventListener("input", debounce(_=>{
@@ -32,5 +67,5 @@ document.addEventListener('wasmload', async function() {
         }))
         console.log(errors)
       }
-    }, 2000))
+    }, TIMEOUT))
 })
