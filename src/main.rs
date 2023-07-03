0d2d1a770d66b7b32d953de086c81c43a9cbdb7b
data-niklas <niklas@4loeser.net>
2023-07-03 16:47:50 +0200

    Revert "update typst version"
    This reverts commit 4e8355a75058a3a98104c920d3b9fa8469e1409c.

--- src/main.rs
+++ src/main.rs
@@ -134,10 +134,6 @@ impl SystemWorld {
 }
 
 impl World for SystemWorld {
-    fn today(&self, offset: Option<i64>) -> Option<typst::eval::Datetime> {
-        Option::None
-    }
-
     fn root(&self) -> &Path {
         &self.root
     }
@@ -155,7 +151,7 @@ impl World for SystemWorld {
     }
 
     fn source(&self, id: SourceId) -> &Source {
-        &self.sources[id.as_u16() as usize]
+        &self.sources[id.into_u16() as usize]
     }
 
     fn book(&self) -> &Prehashed<FontBook> {
